3bf078718f7a280e50f9452b09b370a22c2c3814
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-09-10 23:53:17 +0530

    feat: Table comp structure

--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import TableComp from "../components/TableComp";
 
 function Crypto() {
   return (
     <div>
-      <section></section>
+      <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+        <TableComp />
+      </section>
     </div>
   );
 }
