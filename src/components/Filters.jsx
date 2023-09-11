d9d556e1794427faeaac50e6cf64cae6d7c0811d
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-09-12 01:08:29 +0530

    feat: search comp with debounce mechanism

---
+++ src/components/Filters.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Search from "../components/Search";
+import Sorting from "../components/Sorting";
+import Currency from "../components/Currency";
+
+function Filters() {
+  return (
+    <div className="border-2 width-full rounded-lg h-12 flex items-center relative justify-between">
+      <Search />
+      <Sorting />
+      <Currency />
+    </div>
+  );
+}
+
+export default Filters;
--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Filters from "../components/Filters";
 import TableComp from "../components/TableComp";
 import { CryptoProvider } from "../context/CryptoContext";
 
@@ -6,6 +7,7 @@ function Crypto() {
   return (
     <div>
       <section className=" h-full flex flex-col mt-16 mb-24 relative">
+        <Filters />
         <TableComp />
       </section>
     </div>
