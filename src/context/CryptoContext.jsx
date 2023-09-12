7ac6d339c457196ed5f8555eb77426ca99fea9f8
harshasinamshetty1 <harshasinamshetty1@gmail.com>
2023-09-12 13:35:42 +0530

    feat: working Search comp

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -6,10 +6,13 @@ export const CryptoContext = createContext({});
 export const CryptoProvider = ({ children }) => {
   const [cryptoData, setCryptoData] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  // this is used, when a coin in selected afrer searching
+  const [coinSearch, setCoinSearch] = useState("");
   const getCryptoData = async () => {
     try {
+      //coinSearch param is passed
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&ids=${coinSearch}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en
         `
       )
         .then((res) => res.json())
@@ -31,21 +34,29 @@ export const CryptoProvider = ({ children }) => {
           .then((json) => json);
 
       console.log("data from context= ", data);
-
-      setSearchResults(data);
+      // we are showing results for only coins
+      //but the api has, for evrythng like, markets etc
+      setSearchResults(data.coins);
     } catch (e) {
       console.log("error", e);
     }
   };
   //so that, the data will be loaded before the component renders completely
+  //initially, the coinSearch will be "". therfore we fetch all top coins only. but when, a coin in selected, then we pass that to the api, therfore, only that particular coin data is shown in the table
   useLayoutEffect(() => {
     getCryptoData();
-  }, []);
+  }, [coinSearch]);
 
   // remember to use return,
   return (
     <CryptoContext.Provider
-      value={{ cryptoData, searchResults, getSearchResults }}
+      value={{
+        cryptoData,
+        searchResults,
+        setSearchResults,
+        getSearchResults,
+        setCoinSearch,
+      }}
     >
       {children}
     </CryptoContext.Provider>
