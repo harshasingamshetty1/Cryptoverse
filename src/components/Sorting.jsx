163448ff1e9c769756cdf3beae20b9647b938f49
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-09-13 13:00:21 +0530

    feat: Pagination comp

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useContext } from "react";
 import selectIcon from "../assets/select-icon.svg";
+import { CryptoContext } from "../context/CryptoContext";
 const Sorting = () => {
-  const handleSort = () => {};
+  const { setSortBy } = useContext(CryptoContext);
+  const handleSort = (e) => {
+    e.preventDefault();
+    let val = e.target.value;
+    setSortBy(val);
+  };
   return (
-    <div className="mr-4">
-      <label className="flex">
+    <div className="flex ml-3 mr-7 ">
+      <label className="flex items-center justify-center relative ">
         <span className="font-bold mr-2">sort by: </span>
         <select
           name="sortby"
-          className="bg-gray-200 text-gray-100 relative
-         "
+          className="bg-gray-200  
+         pl-2 pr-10 capitalize focus:outline-0 py-0.5 rounded-lg"
           onClick={handleSort}
         >
           <option value="market_cap_desc">market cap desc</option>
@@ -24,7 +31,9 @@ const Sorting = () => {
         <img
           src={selectIcon}
           alt="submit"
-          className=" absolute right-0 top-4
+          //  the pointer-events-none class will not respond to mouse clicks or other pointer interactions. This is useful when you want to visually overlay an element on top of other content without blocking interaction with the content beneath.
+          // so without this class, when user clicks on the img, the drop down wont show up, bcoz the select tag is beneath the img, therefore now with pointer-events-none, we can click on img, but still the select tag beneath it will be activated!
+          className=" absolute w-[1rem] h-full right-0 pointer-events-none
          "
         />
       </label>
--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -9,12 +9,26 @@ export const CryptoProvider = ({ children }) => {
   // this is used, when a coin in selected afrer searching
   const [coinSearch, setCoinSearch] = useState([]);
   const [currency, setCurrency] = useState("usd");
-
+  const [sortBy, setSortBy] = useState("market_cap_desc");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(250);
+  const [perPage, setPerPage] = useState(10);
   const getCryptoData = async () => {
+    // setTotalPages(13220);
     try {
-      //coinSearch param is passed
+      const data = await fetch(`https://api.coingecko.com/api/v3/coins/list`)
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setTotalPages(data.length);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&ids=${coinSearch}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${sortBy}&per_page=${perPage}&page=${page}&ids=${coinSearch}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en
         `
       )
         .then((res) => res.json())
@@ -47,7 +61,7 @@ export const CryptoProvider = ({ children }) => {
   //initially, the coinSearch will be "". therfore we fetch all top coins only. but when, a coin in selected, then we pass that to the api, therfore, only that particular coin data is shown in the table
   useLayoutEffect(() => {
     getCryptoData();
-  }, [coinSearch, currency]);
+  }, [coinSearch, currency, sortBy, page, perPage]);
 
   // remember to use return,
   return (
@@ -56,6 +70,15 @@ export const CryptoProvider = ({ children }) => {
         cryptoData,
         searchResults,
         currency,
+        sortBy,
+        page,
+        perPage,
+
+        totalPages,
+        setTotalPages,
+        setPage,
+        setPerPage,
+        setSortBy,
         setSearchResults,
         getSearchResults,
         setCoinSearch,
